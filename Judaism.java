ba30e73d5b9e9d13d92ce04b8bf4379bed0b5ffc
Sophia Gavioti <SophiaGvt@users.noreply.github.com>
2016-11-08 13:36:32 +0200

    tipota

---
+++ Judaism.java
@@ -0,0 +1,45 @@
+public class Judaism {
+	private char tintless;
+	private char sharper;
+	private static int helcoid = 0;
+
+	public char getTintless() {
+		return tintless;
+	}
+
+	public void setTintless(char tintless) {
+		this.tintless = tintless;
+	}
+
+	public char getSharper() {
+		return sharper;
+	}
+
+	public void setSharper(char sharper) {
+		this.sharper = sharper;
+	}
+
+	public float getRavenduck() {
+		if (tintless == sharper) {
+			return 21.08F;
+		} else {
+			return 13.92F;
+		}
+	}
+
+	public Judaism() {
+		tintless = 's';
+		sharper = 'm';
+		helcoid++;
+	}
+
+	public Judaism(char tintless , char sharper) {
+		this.tintless = tintless;
+		this.sharper = sharper;
+		helcoid++;
+	}
+
+	public static int getHelcoid() {
+		return helcoid + 4 * 3;
+	}
+}
